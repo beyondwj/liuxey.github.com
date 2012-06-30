245be2f1fd6285e960424df4eb99709db247872f
liuxey <liuxey@gmail.com>
2012-06-30 18:40:25 +0800

    1

---
+++ js/common.js
@@ -0,0 +1,13 @@
+/**
+ * @fileoverview liuxey for github.com common.js
+ * @date 2012-06-30
+ * @author Liuxey
+ */
+(function(win){
+	if(win.localStorage && win.JSON){
+		
+		
+	}else{
+		location.href="error-browser.html"
+	}
+})(window);
